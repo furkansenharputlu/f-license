5c720815f36d51e9c010a9f247f319a0c9a2f76c
furkansenharputlu <furkan_senharputlu@hotmail.com>
2020-03-16 03:14:15 +0300

    Implement Mongo storage

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"f-license/config"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"hash/fnv"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
+	"time"
 )
 
 const Version = "0.1"
@@ -23,20 +28,30 @@ func intro() {
 	logrus.Info("https://f-license.com")
 }
 
-var licenses = make(map[uint64]*License)
+var MongoClient *mongo.Client
+var licensesCol *mongo.Collection
 
 func main() {
 	intro()
 
 	config.Global.Load("config.json")
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	MongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		logrus.Fatalf("Problem while connecting to Mongo: %s", err)
+	}
+
+	licensesCol = MongoClient.Database("f-license").Collection("licenses")
+
 	r := mux.NewRouter()
 	// Endpoints called by product owners
 	adminRouter := r.PathPrefix("/admin").Subrouter()
 	adminRouter.Use(authenticationMiddleware)
 	adminRouter.HandleFunc("/generate", GenerateLicense).Methods(http.MethodPost)
-	adminRouter.HandleFunc("/activate", ChangeLicenseActiveness).Methods(http.MethodPut)
-	adminRouter.HandleFunc("/inactivate", ChangeLicenseActiveness).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{id}", GetLicense).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{id}/activate", ChangeLicenseActiveness).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{id}/inactivate", ChangeLicenseActiveness).Methods(http.MethodPut)
 
 	// Endpoints called by product instances having license
 	r.HandleFunc("/license/verify", VerifyLicense).Methods(http.MethodPost)
@@ -45,17 +60,18 @@ func main() {
 }
 
 type License struct {
-	Type     string        `json:"type"`
-	Hash     uint64        `json:"hash"`
-	Claims   jwt.MapClaims `json:"claims"`
-	Inactive bool          `json:"-"`
+	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Type   string             `bson:"type" json:"type"`
+	Hash   string             `bson:"hash" json:"-"`
+	Token  string             `bson:"token" json:"token"`
+	Claims jwt.MapClaims      `bson:"claims" json:"claims"`
+	Active bool               `bson:"active" json:"active"`
 }
 
 func authenticationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Authorization") != config.Global.AdminSecret {
-			w.WriteHeader(http.StatusUnauthorized)
-			ReturnResponse(w, map[string]interface{}{
+			ReturnResponse(w, http.StatusUnauthorized, map[string]interface{}{
 				"message": "Authorization failed",
 			})
 			return
@@ -66,11 +82,53 @@ func authenticationMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func ReturnResponse(w http.ResponseWriter, resp map[string]interface{}) {
+func ReturnResponse(w http.ResponseWriter, statusCode int, resp map[string]interface{}) {
+	bytes, _ := json.Marshal(resp)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	_, _ = fmt.Fprintf(w, string(bytes))
+}
+
+func ReturnError(w http.ResponseWriter, errMsg string) {
+	logrus.Error(errMsg)
+	resp := map[string]interface{}{
+		"error": errMsg,
+	}
 	bytes, _ := json.Marshal(resp)
 
 	w.Header().Set("Content-Type", "application/json")
 	_, _ = fmt.Fprintf(w, string(bytes))
+}
+
+func GetLicense(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	licenseID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		ReturnError(w, fmt.Sprintf("ID format error: %s", err))
+		return
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	filter := bson.M{"_id": licenseID}
+	res := licensesCol.FindOne(ctx, filter)
+	err = res.Err()
+	if err != nil {
+		ReturnError(w, fmt.Sprintf("error while getting license: %s", err))
+		return
+	}
+
+	var l License
+	_ = res.Decode(&l)
+
+	ReturnResponse(w, 200, map[string]interface{}{
+		"id":     l.ID,
+		"type":   l.Type,
+		"claims": l.Claims,
+		"active": l.Active,
+		"token":  l.Token,
+	})
 }
 
 func GenerateLicense(w http.ResponseWriter, r *http.Request) {
@@ -85,44 +143,80 @@ func GenerateLicense(w http.ResponseWriter, r *http.Request) {
 		logrus.Error("Error signing token:", err)
 	}
 
-	logrus.Info("License successfully generated")
+	l.Token = signedString
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
 	h := fnv.New64a()
 	h.Write([]byte(signedString))
+	l.Hash = fmt.Sprintf("%v", h.Sum64())
 
-	hash := h.Sum64()
+	filter := bson.M{"hash": l.Hash}
+	res := licensesCol.FindOne(ctx, filter)
+	err = res.Err()
+	if err != nil {
+		if err != mongo.ErrNoDocuments {
+			ReturnError(w, fmt.Sprintf("error while checking the existence of license: %s", err))
+			return
+		}
+	} else {
+		var existingLicense License
+		_ = res.Decode(&existingLicense)
+		ReturnError(w, fmt.Sprintf("There is already such license: %s", existingLicense.ID.Hex()))
+		return
+	}
 
-	licenses[hash] = &l
+	l.ID = primitive.NewObjectID()
 
-	ReturnResponse(w, map[string]interface{}{
-		"license":      signedString,
-		"license_hash": hash,
+	update := bson.M{"$set": l}
+	_, err = licensesCol.UpdateOne(ctx, filter, update, options.Update().SetUpsert(true))
+	if err != nil {
+		ReturnError(w, fmt.Sprintf("error while inserting license: %s", err))
+		return
+	}
+
+	logrus.Info("License successfully generated")
+
+	ReturnResponse(w, 200, map[string]interface{}{
+		"id":      l.ID.Hex(),
+		"license": l.Token,
 	})
 }
 
 func ChangeLicenseActiveness(w http.ResponseWriter, r *http.Request) {
-	hash := r.FormValue("license_hash")
-	var message string
+	id := mux.Vars(r)["id"]
 
-	u, _ := strconv.ParseUint(hash, 10, 64)
-	l, ok := licenses[u]
-	if !ok {
-		ReturnResponse(w, map[string]interface{}{
-			"message": "license not found",
-		})
+	licenseID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		ReturnError(w, fmt.Sprintf("ID format error: %s", err))
 		return
 	}
 
-	if strings.HasSuffix(r.URL.Path, "/inactivate") {
-		l.Inactive = true
-		message = "Inactivated"
-		logrus.Infof(`License is successfully inactivated: %d`, u)
-	} else {
-		l.Inactive = false
-		message = "Activated"
-		logrus.Infof(`License is successfully activated: %d`, u)
+	inactivate := strings.HasSuffix(r.URL.Path, "/inactivate")
+
+	filter := bson.M{"_id": bson.M{"$eq": licenseID}}
+	update := bson.M{"$set": bson.M{"active": !inactivate}}
+	res, err := licensesCol.UpdateOne(context.Background(), filter, update)
+	if res.MatchedCount == 0 {
+		ReturnError(w, "There is no matching license")
+		return
+	}
+	if err != nil {
+		ReturnError(w, "License cannot be updated")
+		return
 	}
 
-	ReturnResponse(w, map[string]interface{}{
+	var message string
+
+	if inactivate {
+		message = "Inactivated"
+		logrus.Infof(`License is successfully inactivated: %s`, id)
+	} else {
+		message = "Activated"
+		logrus.Infof(`License is successfully activated: %s`, id)
+	}
+
+	ReturnResponse(w, 200, map[string]interface{}{
 		"message": message,
 	})
 }
@@ -135,7 +229,7 @@ func VerifyLicense(w http.ResponseWriter, r *http.Request) {
 	license := r.FormValue("license")
 	ok, err := IsLicenseValid(license)
 	if err != nil {
-		ReturnResponse(w, map[string]interface{}{
+		ReturnResponse(w, http.StatusUnauthorized, map[string]interface{}{
 			"valid":   false,
 			"message": fmt.Sprintf("error while validating license: %s", err),
 		})
@@ -143,7 +237,7 @@ func VerifyLicense(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ReturnResponse(w, map[string]interface{}{
+	ReturnResponse(w, 200, map[string]interface{}{
 		"valid": ok,
 	})
 }
@@ -151,13 +245,24 @@ func VerifyLicense(w http.ResponseWriter, r *http.Request) {
 func IsLicenseValid(license string) (bool, error) {
 	h := fnv.New64a()
 	h.Write([]byte(license))
+	hash := h.Sum64()
+	hashStr := fmt.Sprintf("%v", hash)
 
-	l, ok := licenses[h.Sum64()]
-	if !ok {
-		return false, fmt.Errorf("license not found")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	filter := bson.M{"hash": hashStr}
+	res := licensesCol.FindOne(ctx, filter)
+	err := res.Err()
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return false, fmt.Errorf("license not found")
+		}
+		return false, fmt.Errorf("error while getting license: %s", err)
 	}
 
-	if l.Inactive {
+	var l License
+	_ = res.Decode(&l)
+
+	if !l.Active {
 		return false, fmt.Errorf("license inactivated")
 	}
 
---
+++ vendor/go.mongodb.org/mongo-driver/x/mongo/driver/topology/sdam_spec_test.go
@@ -0,0 +1,217 @@
+// Copyright (C) MongoDB, Inc. 2017-present.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+package topology
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/internal/testutil/assert"
+	testhelpers "go.mongodb.org/mongo-driver/internal/testutil/helpers"
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/address"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/description"
+)
+
+type response struct {
+	Host     string
+	IsMaster IsMaster
+}
+
+type IsMaster struct {
+	Arbiters                     []string           `bson:"arbiters,omitempty"`
+	ArbiterOnly                  bool               `bson:"arbiterOnly,omitempty"`
+	ClusterTime                  bson.Raw           `bson:"$clusterTime,omitempty"`
+	Compression                  []string           `bson:"compression,omitempty"`
+	ElectionID                   primitive.ObjectID `bson:"electionId,omitempty"`
+	Hidden                       bool               `bson:"hidden,omitempty"`
+	Hosts                        []string           `bson:"hosts,omitempty"`
+	IsMaster                     bool               `bson:"ismaster,omitempty"`
+	IsReplicaSet                 bool               `bson:"isreplicaset,omitempty"`
+	LastWrite                    *lastWriteDate     `bson:"lastWrite,omitempty"`
+	LogicalSessionTimeoutMinutes uint32             `bson:"logicalSessionTimeoutMinutes,omitempty"`
+	MaxBSONObjectSize            uint32             `bson:"maxBsonObjectSize,omitempty"`
+	MaxMessageSizeBytes          uint32             `bson:"maxMessageSizeBytes,omitempty"`
+	MaxWriteBatchSize            uint32             `bson:"maxWriteBatchSize,omitempty"`
+	Me                           string             `bson:"me,omitempty"`
+	MaxWireVersion               int32              `bson:"maxWireVersion,omitempty"`
+	MinWireVersion               int32              `bson:"minWireVersion,omitempty"`
+	Msg                          string             `bson:"msg,omitempty"`
+	OK                           int32              `bson:"ok"`
+	Passives                     []string           `bson:"passives,omitempty"`
+	ReadOnly                     bool               `bson:"readOnly,omitempty"`
+	SaslSupportedMechs           []string           `bson:"saslSupportedMechs,omitempty"`
+	Secondary                    bool               `bson:"secondary,omitempty"`
+	SetName                      string             `bson:"setName,omitempty"`
+	SetVersion                   uint32             `bson:"setVersion,omitempty"`
+	Tags                         map[string]string  `bson:"tags,omitempty"`
+}
+
+type lastWriteDate struct {
+	LastWriteDate time.Time `bson:"lastWriteDate"`
+}
+
+type server struct {
+	Type           string
+	SetName        string
+	SetVersion     uint32
+	ElectionID     *primitive.ObjectID `bson:"electionId"`
+	MinWireVersion *int32
+	MaxWireVersion *int32
+}
+
+type outcome struct {
+	Servers                      map[string]server
+	TopologyType                 string
+	SetName                      string
+	LogicalSessionTimeoutMinutes uint32
+	MaxSetVersion                uint32
+	MaxElectionID                primitive.ObjectID `bson:"maxElectionId"`
+	Compatible                   *bool
+}
+
+type phase struct {
+	Responses []response
+	Outcome   outcome
+}
+
+type testCase struct {
+	Description string
+	URI         string
+	Phases      []phase
+}
+
+const testsDir string = "../../../../data/server-discovery-and-monitoring/"
+
+func (r *response) UnmarshalJSON(buf []byte) error {
+	tmp := []interface{}{&r.Host, &r.IsMaster}
+	if err := json.Unmarshal(buf, &tmp); err != nil {
+		return err
+	}
+
+	if len(tmp) != 2 {
+		return fmt.Errorf("'response' JSON array must have exactly two elements")
+	}
+
+	return nil
+}
+
+func setUpFSM(t *testing.T, uri string) *fsm {
+	fsm := newFSM()
+	cs, err := connstring.ParseAndValidate(uri)
+	assert.Nil(t, err, "Parse error: %v", err)
+
+	fsm.SetName = cs.ReplicaSet
+	if fsm.SetName != "" {
+		fsm.Kind = description.ReplicaSetNoPrimary
+	} else if len(cs.Hosts) == 1 {
+		fsm.Kind = description.Single
+	}
+
+	for _, host := range cs.Hosts {
+		fsm.Servers = append(fsm.Servers, description.Server{Addr: address.Address(host).Canonicalize()})
+	}
+
+	return fsm
+}
+
+func applyResponses(f *fsm, responses []response) error {
+	for _, response := range responses {
+		doc, err := bson.Marshal(response.IsMaster)
+		if err != nil {
+			return err
+		}
+		server := description.NewServer(address.Address(response.Host), bsoncore.Document(doc))
+		_, err = f.apply(server)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func runTest(t *testing.T, directory string, filename string) {
+	filepath := path.Join(testsDir, directory, filename)
+	content, err := ioutil.ReadFile(filepath)
+	assert.Nil(t, err, "ReadFile error: %v", err)
+
+	// Remove ".json" from filename.
+	filename = filename[:len(filename)-5]
+	testName := directory + "/" + filename + ":"
+
+	t.Run(testName, func(t *testing.T) {
+		var test testCase
+		err = json.Unmarshal(content, &test)
+		assert.Nil(t, err, "Unmarshal error: %v", err)
+		f := setUpFSM(t, test.URI)
+
+		for _, phase := range test.Phases {
+			err = applyResponses(f, phase.Responses)
+			if phase.Outcome.Compatible == nil || *phase.Outcome.Compatible {
+				assert.Nil(t, err, "error: %v", err)
+			} else {
+				assert.NotNil(t, err, "Expected error")
+				continue
+			}
+
+			assert.Equal(t, phase.Outcome.TopologyType, f.Kind.String(),
+				"expected TopologyType to be %v, got %v", phase.Outcome.TopologyType, f.Kind.String())
+			assert.Equal(t, phase.Outcome.SetName, f.SetName,
+				"expected SetName to be %v, got %v", phase.Outcome.SetName, f.SetName)
+			assert.Equal(t, len(phase.Outcome.Servers), len(f.Servers),
+				"expected %v servers, got %v", len(phase.Outcome.Servers), len(f.Servers))
+			assert.Equal(t, phase.Outcome.LogicalSessionTimeoutMinutes, f.SessionTimeoutMinutes,
+				"expected SessionTimeoutMinutes to be %v, got %v", phase.Outcome.LogicalSessionTimeoutMinutes, f.SessionTimeoutMinutes)
+			assert.Equal(t, phase.Outcome.MaxSetVersion, f.maxSetVersion,
+				"expected maxSetVersion to be %v, got %v", phase.Outcome.MaxSetVersion, f.maxSetVersion)
+			assert.Equal(t, phase.Outcome.MaxElectionID, f.maxElectionID,
+				"expected maxElectionID to be %v, got %v", phase.Outcome.MaxElectionID, f.maxElectionID)
+
+			for addr, server := range phase.Outcome.Servers {
+				fsmServer, ok := f.Server(address.Address(addr))
+				assert.True(t, ok, "Couldn't find server %v", addr)
+
+				assert.Equal(t, address.Address(addr), fsmServer.Addr,
+					"expected server address to be %v, got %v", address.Address(addr), fsmServer.Addr)
+				assert.Equal(t, server.SetName, fsmServer.SetName,
+					"expected server SetName to be %v, got %v", server.SetName, fsmServer.SetName)
+				assert.Equal(t, server.SetVersion, fsmServer.SetVersion,
+					"expected server SetVersion to be %v, got %v", server.SetVersion, fsmServer.SetVersion)
+				if server.ElectionID != nil {
+					assert.Equal(t, *server.ElectionID, fsmServer.ElectionID,
+						"expected server ElectionID to be %v, got %v", *server.ElectionID, fsmServer.ElectionID)
+				}
+
+				// PossiblePrimary is only relevant to single-threaded drivers.
+				if server.Type == "PossiblePrimary" {
+					server.Type = "Unknown"
+				}
+
+				assert.Equal(t, server.Type, fsmServer.Kind.String(),
+					"expected server Type to be %v, got %v", server.Type, fsmServer.Kind.String())
+			}
+		}
+	})
+}
+
+// Test case for all SDAM spec tests.
+func TestSDAMSpec(t *testing.T) {
+	for _, subdir := range []string{"single", "rs", "sharded"} {
+		for _, file := range testhelpers.FindJSONFilesInDir(t, path.Join(testsDir, subdir)) {
+			runTest(t, subdir, file)
+		}
+	}
+}
